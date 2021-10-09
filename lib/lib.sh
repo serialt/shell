0761b322dcb525f7139763742418291bb62057fc
serialt <serialt@126.com>
2021-10-09 09:43:21 +0800

    feat：优化history输出

--- lib/lib.sh
+++ lib/lib.sh
@@ -248,3 +248,14 @@ check_port() {
     fi
 }
 
+
+#处理历史命令结果
+function history(){
+	if ! grep "HISTTIMEFORMAT" /etc/profile >/dev/null 2>&1
+	then echo '
+	UserIP=$(who -u am i | cut -d"("  -f 2 | sed -e "s/[()]//g")
+	export HISTTIMEFORMAT="[%F %T] [`whoami`] [${UserIP}] " ' >> /etc/profile;
+	fi
+	sed -i "s/HISTSIZE=1000/HISTSIZE=999999999/" /etc/profile
+    echo "[history 优化] ==> OK"
+}
